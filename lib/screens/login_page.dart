0aaa4e7ee9502ecf602ce707e0e59494bc4c1c9d
Ida Made Revindra Dikta Mahendra <idamaderevindra05@gmail.com>
2024-11-27 00:50:26 +0700

    Define Login Page (1 To-do)

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,14 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:ristek_pay/screens/home_page.dart';
+import 'package:ristek_pay/widgets/input_box.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('This is the Login Page'),
-      ),
-    );
+    TextEditingController nameController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+
+    return Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Ristek White Logo
+              Image.asset(
+                'assets/images/white-logo.png',
+                height: 140.0,
+              ),
+
+              // Spacing
+              const SizedBox(
+                height: 54.0,
+              ),
+
+              // Name Text Field
+              InputBox(
+                InputCard(
+                  title: 'Nama',
+                  hintText: 'e.g. Budi Santoso',
+                  isPassword: false,
+                  controller: nameController,
+                ),
+              ),
+
+              // Spacing
+              const SizedBox(
+                height: 16.0,
+              ),
+
+              // Password Text Field
+              InputBox(
+                InputCard(
+                  title: 'Password',
+                  hintText: 'e.g. bUd1S4nt0s0',
+                  isPassword: true,
+                  controller: passwordController,
+                ),
+              ),
+
+              // Spacing
+              const SizedBox(
+                height: 100.0,
+              ),
+
+              // Login Button
+              ElevatedButton(
+                onPressed: () {
+                  if (nameController.text == 'Ristek' &&
+                      passwordController.text == '123456') {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (context) => const HomePage(),
+                      ),
+                    );
+                  } else {
+                    // TODO: Fix Alert Dialog
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text('Login Failed'),
+                          content: const Text('Please check your credentials'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('OK'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.tertiary,
+                  minimumSize: const Size(295.0, 48.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  textStyle: const TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w700,
+                    fontFamily: 'Inter',
+                  ),
+                ),
+                child: const Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.black,
+                    fontFamily: 'Inter',
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
---
+++ lib/widgets/input_box.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class InputCard {
+  final String title;
+  final String hintText;
+  final bool isPassword;
+  TextEditingController controller = TextEditingController();
+
+  InputCard({
+    required this.title,
+    required this.hintText,
+    required this.isPassword,
+    required this.controller,
+  });
+}
+
+class InputBox extends StatelessWidget {
+  final InputCard inputCard;
+
+  const InputBox(this.inputCard, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 295.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            inputCard.title,
+            style: TextStyle(
+              fontSize: 14.0,
+              fontWeight: FontWeight.w500,
+              color: Theme.of(context).colorScheme.tertiary,
+              fontFamily: 'Inter',
+            ),
+          ),
+          // Spacing
+          const SizedBox(
+            height: 8.0,
+          ),
+          TextFormField(
+            controller: inputCard.controller,
+            decoration: InputDecoration(
+              hintText: inputCard.hintText,
+              hintStyle: const TextStyle(
+                fontSize: 14.0,
+                fontWeight: FontWeight.w400,
+                fontFamily: 'Inter',
+              ),
+              filled: true,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12.0),
+                borderSide: BorderSide.none,
+              ),
+            ),
+            obscureText: inputCard.isPassword,
+          )
+        ],
+      ),
+    );
+  }
+}
