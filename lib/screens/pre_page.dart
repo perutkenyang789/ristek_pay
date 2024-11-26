98caaa5dfee63a0ec7fbc8b74942f473bfd66e33
Ida Made Revindra Dikta Mahendra <idamaderevindra05@gmail.com>
2024-11-26 23:49:25 +0700

    Define Pre Page

--- lib/screens/pre_page.dart
+++ lib/screens/pre_page.dart
@@ -1,14 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:ristek_pay/screens/login_page.dart';
 
 class PrePage extends StatelessWidget {
   const PrePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('This is the Pre Page'),
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: MediaQuery.of(context).size.height *
+              0.13, // Padding to be 13% of the screen height
+          horizontal: MediaQuery.of(context).size.width *
+              0.13, // Padding to be 13% of the screen width
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Ristek Purple Logo
+            Image.asset(
+              'assets/images/purple-logo.png',
+              height: 140.0,
+            ),
+
+            // Spacing
+            const SizedBox(
+              height: 54.0,
+            ),
+
+            // Ristek Pay Title
+            Text(
+              'RISTEK Pay',
+              style: TextStyle(
+                fontSize: 40.58,
+                fontWeight: FontWeight.w800,
+                color: Theme.of(context).colorScheme.secondary,
+                fontFamily: 'Inter',
+              ),
+            ),
+
+            // Spacing
+            const SizedBox(
+              height: 17.0,
+            ),
+
+            // Ristek Tagline
+            const Text(
+              "#RemarkableEscalation",
+              style: TextStyle(
+                fontSize: 18.55,
+                fontWeight: FontWeight.w400,
+                color: Colors.black,
+                fontFamily: 'Inter',
+              ),
+            ),
+
+            // Spacing
+            const SizedBox(
+              height: 70.0,
+            ),
+
+            // Next Button
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const LoginPage()));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.secondary,
+                  minimumSize: const Size(double.infinity, 50.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                child: Text(
+                  'Next',
+                  style: TextStyle(
+                    fontSize: 18.55,
+                    fontWeight: FontWeight.w600,
+                    color: Theme.of(context).colorScheme.tertiary,
+                    fontFamily: 'Inter',
+                  ),
+                )),
+          ],
+        ),
       ),
+      backgroundColor: Theme.of(context).colorScheme.tertiary,
     );
   }
 }
